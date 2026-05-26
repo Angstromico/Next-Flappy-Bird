23a35f53a207b7c9a32904aa29d5172839b9fd5d
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-26 08:55:31 -0400

    feat(component): generate and use canvas component

---
+++ src/app/components/Canva.tsx
@@ -0,0 +1,7 @@
+const Canva = () => {
+  return (
+    <canvas width="400" height="600"></canvas>
+  )
+}
+
+export default Canva
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Canva from "./components/Canva";
+
 export default function Home() {
   return (
     <div>
       <main>
-        
+        <Canva />
       </main>
     </div>
   );
