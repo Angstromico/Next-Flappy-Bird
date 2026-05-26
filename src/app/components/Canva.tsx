1970d30d15ae260ee5fde81dfe58de7278679be3
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-26 09:03:05 -0400

    feat(canvas): set basic settings up

--- src/app/components/Canva.tsx
+++ src/app/components/Canva.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 const Canva = () => {
+ const context = (document.querySelector('#gameCanvas') as HTMLCanvasElement)?.getContext('2d')
+ if (!context) return null
+ context.canvas.width = 400
+ context.canvas.height = 600
+
   return (
-    <canvas width="400" height="600"></canvas>
+    <canvas id="gameCanvas"></canvas>
   )
 }
 
